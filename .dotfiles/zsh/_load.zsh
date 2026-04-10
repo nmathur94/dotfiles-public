4fca8bda5d6fae1594662b85721af7bf2a46f315
Neil Mathur <26237805+nmathur94@users.noreply.github.com>
2026-04-10 19:10:25 +1000

    Updated functions
    - moved custom files from root zsh folder into parts
    - added a loader file that will control order of files to load

---
+++ .dotfiles/zsh/_load.zsh
@@ -0,0 +1,5 @@
+source "$ZSH_CUSTOM/parts/function.zsh"
+source "$ZSH_CUSTOM/parts/profile.zsh"
+source "$ZSH_CUSTOM/parts/alias.zsh"
+
+# local.zsh is sourced automatically by Oh My Zsh after this file
